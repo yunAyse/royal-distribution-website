618bb26663bc1a5a6c0104b3c54847c20199010f
Ayse <115019212+yunAyse@users.noreply.github.com>
2023-09-19 14:35:34 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const navbar = document.querySelector(".navbar");
+
+//! buttons 
+
+const menuBtn = document.querySelector("#menu-btn");
+
+menuBtn.addEventListener("click", function () {
+  navbar.classList.toggle("active");
+  document.addEventListener("click", function (e) {
+    if (
+      !e.composedPath().includes(menuBtn) &&
+      !e.composedPath().includes(navbar)
+    ) {
+      navbar.classList.remove("active");
+    }
+  });
+});
